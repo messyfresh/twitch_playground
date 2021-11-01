76612b7c9c6535e9eff4d2fdae88c67d86811986
Scott Nix <messyfresh@gmail.com>
2021-11-01 18:27:01 -0500

    Restructure

---
+++ wemo_control.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/bin/python /home/messyfresh/twitch_projects/wemo_control/client.py --device 192.168.10.30 --$1
---
+++ wemo_flash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+REPS=0
+STATE=off
+
+while [ $REPS -le 12 ]
+do
+    #/usr/bin/python /home/messyfresh/twitch_projects/wemo_control/client.py --device 192.168.10.30 --$STATE
+    /home/messyfresh/wemo_control.sh $STATE
+    REPS=$((REPS+1))
+    if [ $STATE == "off" ]
+    then
+        STATE=on
+    else
+        STATE=off
+    fi
+    sleep 0.5
+done
+
+/usr/bin/python /home/messyfresh/twitch_projects/wemo_control/client.py --device 192.168.10.30 --on

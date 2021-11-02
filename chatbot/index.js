f1880ce5f64c00cdc4b1768787fbaa7b67d3f62e
Scott Nix <messyfresh@gmail.com>
2021-11-02 00:52:25 -0500

    Chat bot working; !ping and !dice commands

--- chatbot/index.js
+++ chatbot/index.js
@@ -2,29 +2,34 @@ import { RefreshingAuthProvider } from '@twurple/auth';
 import { ChatClient } from '@twurple/chat';
 import { promises as fs } from 'fs';
 
+const TwitchConfig = JSON.parse(await fs.readFile('./config/twitch.json', 'UTF-8'));
+
 async function main() {
-	// TODO Import these from .json file
-    const clientId = 'YOUR_CLIENT_ID';
-    const clientSecret = 'YOUR_CLIENT_SECRET';
-    const tokenData = JSON.parse(await fs.readFile('./tokens.json', 'UTF-8'));
+    // Auth
+    const clientId = TwitchConfig.ClientId;
+    const clientSecret = TwitchConfig.ClientSecret;
+    const tokenData = JSON.parse(await fs.readFile('./config/token.json', 'UTF-8'));
     const authProvider = new RefreshingAuthProvider(
         {
             clientId,
             clientSecret,
-            onRefresh: async newTokenData => await fs.writeFile('./tokens.json', JSON.stringify(newTokenData, null, 4), 'UTF-8')
+            onRefresh: async newTokenData => {
+                await fs.writeFile('./config/token.json', JSON.stringify(newTokenData, null, 4), 'UTF-8');
+                console.log("Token Refreshed: ", newTokenData);
+            }
         },
         tokenData
     );
 
     // Chat
-    const chatClient = new ChatClient({ authProvider, channels: ['satisfiedpear'] });
+    const chatClient = new ChatClient({ authProvider, channels: ['messyfresh'] });
     await chatClient.connect();
 
     chatClient.onMessage((channel, user, message) => {
         if (message === '!ping') {
             chatClient.say(channel, 'Pong!');
         } else if (message === '!dice') {
-            const diceRoll = Math.floor(Math.random() * 6) + 1;
+            const diceRoll = Math.floor(Math.random() * 20) + 1;
             chatClient.say(channel, `@${user} rolled a ${diceRoll}`)
         }
     });
